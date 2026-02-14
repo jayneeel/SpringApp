cc1e53b7522b8b6200f25a9380ac33a976fe3514
Jayneel <jayneelkanungo57@gmail.com>
2026-02-15 00:44:52 +0530

    dependency injection

--- src/main/java/com/jayneel/SpringApp/SpringAppApplication.java
+++ src/main/java/com/jayneel/SpringApp/SpringAppApplication.java
@@ -2,13 +2,16 @@ package com.jayneel.SpringApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringAppApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SpringAppApplication.class, args);
 		System.out.println("Hello World");
+		Alien alien = context.getBean(Alien.class);
+		alien.code();
 	}
 
 }
