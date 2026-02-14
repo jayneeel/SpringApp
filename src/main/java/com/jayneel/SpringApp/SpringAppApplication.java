920a6c1c6bef2b924a7732ff2f91d828dfc5b929
Jayneel <jayneelkanungo57@gmail.com>
2026-02-15 00:30:52 +0530

    git init

---
+++ src/main/java/com/jayneel/SpringApp/SpringAppApplication.java
@@ -0,0 +1,14 @@
+package com.jayneel.SpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
