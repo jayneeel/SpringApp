89b3ff9fe0ef3a537d6202bd0161100ee734f676
Jayneel <jayneelkanungo57@gmail.com>
2026-02-15 00:46:25 +0530

    auto wiring

--- src/main/java/com/jayneel/SpringApp/Alien.java
+++ src/main/java/com/jayneel/SpringApp/Alien.java
@@ -1,12 +1,17 @@
 package com.jayneel.SpringApp;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Alien {
 
+    @Autowired
+    Laptop laptop;
+
     public void code() {
+        laptop.compile();
         System.out.println("Coding....");
     }
 }
---
+++ src/main/java/com/jayneel/SpringApp/Laptop.java
@@ -0,0 +1,11 @@
+package com.jayneel.SpringApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+    public void compile(){
+        System.out.println("Compiling...");
+    }
+}
